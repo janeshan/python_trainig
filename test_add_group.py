cc4b3de9133cde2aa9e931c8bb56c664e37128ac
Jane <yanafinl@gmail.com>
2020-06-10 22:49:51 +0300

    delete not used code

--- test_add_group.py
+++ test_add_group.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
 import unittest
 from group import Group
 
@@ -61,16 +59,6 @@ class TestAddGroup(unittest.TestCase):
         self.return_to_group_page(wd)
         self.logout(wd)
 
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
     def tearDown(self):
         self.wd.quit()
 
