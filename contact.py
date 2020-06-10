590227e9f2e21888541f543a29a6bcdefa9e9ccb
Jane <yanafinl@gmail.com>
2020-06-11 00:42:59 +0300

    fis

---
+++ contact.py
@@ -0,0 +1,24 @@
+class Contact:
+
+    def __init__(self, firstname, middlename, secondname, nickname, title, company, address_1, tel_1, tel_2, tel_3, mail_1,
+                              mail_2, mail_3, byear, year, address_2, home, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.secondname = secondname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address_1 = address_1
+        self.tel_1 = tel_1
+        self.tel_2 = tel_2
+        self.tel_3 = tel_3
+        self.mail_1 = mail_1
+        self.mail_2 = mail_2
+        self.mail_3 = mail_3
+        self.byear = byear
+        self.year = year
+        self.home = home
+        self.notes = notes
+        self.address_2 = address_2
+
+
