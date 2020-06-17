d5fd5ef432e4143f13e82e1f1708df8686f058f9
Jane <yanafinl@gmail.com>
2020-06-18 00:13:43 +0300

    fix2

--- fixture/group.py
+++ fixture/group.py
@@ -5,8 +5,14 @@ class GroupHelper:
         self.app = app
 
 
+    def open_group_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("groups").click()
+
+
     def create(self, group):
         wd = self.app.wd
+        self.open_group_page()
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -25,6 +31,7 @@ class GroupHelper:
 
     def delete_first_group(self):
         wd = self.app.wd
+        self.open_group_page()
         # delete first group
         wd.find_element_by_name("selected[]").click()
         # submit deletion
