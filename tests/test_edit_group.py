7baf5ce6fae05ace4c3bf790553c8eb76f3bb938
Jane <yanafinl@gmail.com>
2020-06-27 20:24:43 +0300

    добавление групы или контакта перед редактированием

--- tests/test_edit_group.py
+++ tests/test_edit_group.py
@@ -1,4 +1,6 @@
 from model.group import Group
 
 def test_edit_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="", header="", footer=""))
     app.group.edit_group(Group(name="edited", header="edited", footer="edited"))
