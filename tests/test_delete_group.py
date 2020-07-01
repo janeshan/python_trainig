31497fc6e43e205e4a747dd0dc6469581f7ba11c
Jane <yanafinl@gmail.com>
2020-07-01 14:12:33 +0300

    добавлена проверка равенства списков для групп

--- tests/test_delete_group.py
+++ tests/test_delete_group.py
@@ -7,6 +7,8 @@ def test_delete_first_group(app):
     app.group.delete_first_group()
     new_group_list = app.group.get_group_list()
     assert len(old_group_list) - 1 == len(new_group_list)
+    old_group_list[0:1] = []
+    assert old_group_list == new_group_list
 
 
 
