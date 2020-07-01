daa03f62e10271abf5bb80e59cdb2588091beddd
Jane <yanafinl@gmail.com>
2020-07-01 15:03:09 +0300

    добавлена сортировка списков  для групп

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -3,18 +3,25 @@
 from model.group import Group
 
 
+
 def test_add_group(app):
     old_group_list = app.group.get_group_list()
-    app.group.create(Group(name="New my group", header="my header", footer="my footer"))
+    group = Group(name="New my group", header="my header", footer="my footer")
+    app.group.create(group)
     new_group_list = app.group.get_group_list()
     assert len(old_group_list) + 1 == len(new_group_list)
+    old_group_list.append(group)
+    assert sorted(old_group_list, key=Group.id_or_max) == sorted(new_group_list, key=Group.id_or_max)
     #app.session.logout()
 
 
 def test_add_empty_group(app):
     old_group_list = app.group.get_group_list()
-    app.group.create(Group(name="", header="", footer=""))
+    group = Group(name="", header="", footer="")
+    app.group.create(group)
     new_group_list = app.group.get_group_list()
     assert len(old_group_list) + 1 == len(new_group_list)
+    old_group_list.append(group)
+    assert sorted(old_group_list, key=Group.id_or_max) == sorted(new_group_list, key=Group.id_or_max)
 
 
