e8ed957b2d6c59c544a2b00d3f8f5448cca29d1c
Jane <yanafinl@gmail.com>
2020-07-01 15:14:24 +0300

    добавлена сортировка списков  для редактирования групп

--- tests/test_edit_group.py
+++ tests/test_edit_group.py
@@ -4,6 +4,10 @@ def test_edit_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="", header="", footer=""))
     old_group_list = app.group.get_group_list()
-    app.group.edit_group(Group(name="edited", header="edited", footer="edited"))
+    group = Group(name="edited", header="edited", footer="edited")
+    group.id = old_group_list[0].id
+    app.group.edit_group(group)
     new_group_list = app.group.get_group_list()
     assert len(old_group_list) == len(new_group_list)
+    old_group_list[0] = group
+    assert sorted(old_group_list, key=Group.id_or_max) == sorted(new_group_list, key=Group.id_or_max)
