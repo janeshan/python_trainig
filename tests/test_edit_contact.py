e25dd7833a587cac8f19305f9412766ada72fa2c
Jane <yanafinl@gmail.com>
2020-07-08 21:22:01 +0300

    сравнение списков до и после редактирования контактов

--- tests/test_edit_contact.py
+++ tests/test_edit_contact.py
@@ -12,8 +12,10 @@ def test_edit_contact(app):
                                    home="1",
                                    notes="привет! друзья"))
         app.contact.submit_contact_creation()
+    old_contact_list = app.contact.get_contact_list()
     app.contact.edit_contact(Contact(firstname="Vika", middlename="Ivanovna", secondname="Kolinka", nickname="vikul", title="title", company="Flower",
                                address_1="Kazan Tatarstan st 5", tel_1="89111111111", tel_2="89222222222", tel_3="87777777777", mail_1="1first@mail.ru",
                                mail_2="2second@mail.ru", mail_3="3third@mail.ru", bday="18", bmonth="March", byear="1989", day="18", month="March", year="2020", address_2="Kazan Chistopol 11", home="1",
                                notes="Hi!"))
-
+    new_contact_list = app.contact.get_contact_list()
+    assert len(old_contact_list) == len(new_contact_list)
