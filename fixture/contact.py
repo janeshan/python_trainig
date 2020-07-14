49af15115441db340911a7b2a57b1a1430c320e1
Jane <yanafinl@gmail.com>
2020-07-15 01:24:31 +0300

    удаляется случайно выбранный элемент в контактах

--- fixture/contact.py
+++ fixture/contact.py
@@ -73,10 +73,21 @@ class ContactHelper:
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
         self.return_home_page()
 
+    def select_first_contact(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+
+    def select_contact_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
+
     def delete_first_contact(self):
+        self.delete_contact_by_index(0)
+
+    def delete_contact_by_index(self, index):
         wd = self.app.wd
         self.open_home_page()
-        wd.find_element_by_name("selected[]").click()
+        self.select_contact_by_index(index)
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.switch_to_alert().accept()
         self.contact_cache = None
--- tests/test_delete_contact.py
+++ tests/test_delete_contact.py
@@ -1,4 +1,5 @@
 from model.contact import Contact
+from random import randrange
 
 def test_delete_contact(app):
     if app.contact.count() == 0:
@@ -12,6 +13,7 @@ def test_delete_contact(app):
                                    notes="привет! друзья"))
         app.contact.submit_contact_creation()
     old_contact_list = app.contact.get_contact_list()
-    app.contact.delete_first_contact()
+    index = randrange(len(old_contact_list))
+    app.contact.delete_contact_by_index(index)
     new_contact_list = app.contact.get_contact_list()
     assert len(old_contact_list) - 1 == len(new_contact_list)
