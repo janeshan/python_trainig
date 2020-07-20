b89b7ae3824ae3b34c0f023aec68b8d3e0ec0f85
Jane <yanafinl@gmail.com>
2020-07-21 00:06:08 +0300

    добавлена проверка списков после удаления контактов

--- tests/test_delete_contact.py
+++ tests/test_delete_contact.py
@@ -17,3 +17,5 @@ def test_delete_contact(app):
     app.contact.delete_contact_by_index(index)
     new_contact_list = app.contact.get_contact_list()
     assert len(old_contact_list) - 1 == len(new_contact_list)
+    old_contact_list[index:index + 1] = []
+    assert old_contact_list == new_contact_list
