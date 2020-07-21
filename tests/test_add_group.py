785ea1d2315ff4aa059dae4501babcf60a94e6f7
Jane <yanafinl@gmail.com>
2020-07-22 02:33:18 +0300

    отредактирован

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -26,13 +26,5 @@ def test_add_group(app, group):
     #app.session.logout()
 
 
-# def test_add_empty_group(app):
-#     old_group_list = app.group.get_group_list()
-#     group = Group(name="", header="", footer="")
-#     app.group.create(group)
-#     new_group_list = app.group.get_group_list()
-#     assert len(old_group_list) + 1 == len(new_group_list)
-#     old_group_list.append(group)
-#     assert sorted(old_group_list, key=Group.id_or_max) == sorted(new_group_list, key=Group.id_or_max)
 
 
