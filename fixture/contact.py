5ead5ab2a5645650ae7061b6f1138da6dcd08c1e
Jane <yanafinl@gmail.com>
2020-07-22 01:06:48 +0300

    тест сравнения телефонов на старнице просмотра

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,4 +1,5 @@
 from model.contact import Contact
+import re
 
 class ContactHelper:
     def __init__(self, app):
@@ -103,6 +104,13 @@ class ContactHelper:
         listel = el.find_elements_by_tag_name("td")[7]
         listel.find_element_by_tag_name("a").click()
 
+    def open_contact_view_by_index(self, index):
+        wd = self.app.wd
+        self.open_home_page()
+        # contact edition
+        el = wd.find_elements_by_name("entry")[index]
+        listel = el.find_elements_by_tag_name("td")[6]
+        listel.find_element_by_tag_name("a").click()
 
     def edit_contact_by_index(self, index, contact):
         wd = self.app.wd
@@ -158,6 +166,18 @@ class ContactHelper:
         return Contact(firstname=firstname, secondname=secondname, id=id,
                         homephone=homephone, workphone=workphone,
                         mobilephone=mobilephone, secondaryphone=secondaryphone)
+
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        homephone = re.search("H: (.*)", text).group(1)
+        mobilephone = re.search("M: (.*)", text).group(1)
+        workphone = re.search("W: (.*)", text).group(1)
+        secondaryphone = re.search("P: (.*)", text).group(1)
+        return Contact(homephone=homephone, workphone=workphone,
+                       mobilephone=mobilephone, secondaryphone=secondaryphone)
+
 
 
 
