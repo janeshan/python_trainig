5be7571d4650f89aa092c81d7ca48dd3f84657e0
Jane <yanafinl@gmail.com>
2020-07-23 01:27:02 +0300

    параметризованные тесты для создания контакта

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -2,14 +2,40 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 from model.contact import Contact
+import pytest
+import random
+import string
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*5
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+def random_phone(maxlen):
+    symbols = string.digits + " " + "-"
+    return "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+def random_email(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))]) + "@gmail.com"
 
 
-def test_add_contact(app):
+
+testdata = [Contact(firstname="", middlename="", secondname="", nickname="", title="", company="", address_1="",
+                    homephone="", mobilephone="", workphone="", mail_1="", mail_2="", mail_3="", address_2="",
+                    secondaryphone="", notes="")] + [
+    Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 20), secondname=random_string("secondname", 20),
+            nickname=random_string("nickname", 20), title=random_string("title", 20), company=random_string("company", 20),
+            address_1=random_string("address_1", 20), homephone=random_phone(15), mobilephone=random_phone(15),
+            workphone=random_phone(15), mail_1=random_email("mail_1", 20), mail_2=random_email("mail_2", 20),
+            mail_3=random_email("mail_3", 20), address_2=random_string("address_2", 20), secondaryphone=random_phone(15),
+            notes=random_string("notes", 20))
+    for i in range(2)
+    ]
+
+
+@pytest.mark.parametrize("contact", testdata, ids = [repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contact_list = app.contact.get_contact_list()
-    contact = Contact(firstname="Николай", middlename="Иванович", secondname="Иванушкин", nickname="ivan", title="заголовок", company="Ромашка",
-                               address_1="казань чистопольская 33", homephone="89000000000", mobilephone="88888888888", workphone="87777777777", mail_1="first@mail.ru",
-                               mail_2="second@mail.ru", mail_3="third@mail.ru", bday="18", bmonth="March", byear="1989", day="18", month="March", year="2020", address_2="казань петербургская", secondaryphone="86666666666",
-                               notes="привет! друзья")
     app.contact.create(contact)
     app.contact.submit_contact_creation()
     assert len(old_contact_list) + 1 == app.contact.count()
