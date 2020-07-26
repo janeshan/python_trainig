e7561c9e89abc5f7f8926003e71f55e71f4ca1db
Jane <yanafinl@gmail.com>
2020-07-26 19:52:59 +0300

    использование jsonpickle

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -3,8 +3,8 @@
 from model.group import Group
 
 
-def test_add_group(app, data_groups):
-    group = data_groups
+def test_add_group(app, json_groups):
+    group = json_groups
     old_group_list = app.group.get_group_list()
     app.group.create(group)
     assert len(old_group_list) + 1 == app.group.count()
