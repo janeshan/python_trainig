279a92ca915bba765b6b15a209e2307681975651
Jane <yanafinl@gmail.com>
2020-07-26 19:41:20 +0300

    динамическая генерация тестов

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 #import unittest
 from model.group import Group
-import pytest
-from data.add_group import testdata
 
 
-@pytest.mark.parametrize("group", testdata, ids = [repr(x) for x in testdata])
-def test_add_group(app, group):
+def test_add_group(app, data_groups):
+    group = data_groups
     old_group_list = app.group.get_group_list()
     app.group.create(group)
     assert len(old_group_list) + 1 == app.group.count()
