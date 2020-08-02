7064bedb198257c3254fb4a7ce2971e75bfbea6b
Jane <yanafinl@gmail.com>
2020-08-02 14:29:03 +0300

    сранвение данных для добавления / удаления групп из бд

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -3,12 +3,11 @@
 from model.group import Group
 
 
-def test_add_group(app, json_groups):
+def test_add_group(app, db, json_groups):
     group = json_groups
-    old_group_list = app.group.get_group_list()
+    old_group_list = db.get_group_list()
     app.group.create(group)
-    assert len(old_group_list) + 1 == app.group.count()
-    new_group_list = app.group.get_group_list()
+    new_group_list = db.get_group_list()
     old_group_list.append(group)
     assert sorted(old_group_list, key=Group.id_or_max) == sorted(new_group_list, key=Group.id_or_max)
     #app.session.logout()
