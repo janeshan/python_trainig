713da8e2f261f46ebc7fc17065e5d9209cff93a6
Jane <yanafinl@gmail.com>
2020-08-02 19:03:10 +0300

    выборка значений из бд

--- fixture/db.py
+++ fixture/db.py
@@ -29,7 +29,7 @@ class DBFixture:
         cursor = self.connection.cursor()
         try:
             cursor.execute("select id, firstname, middlename, lastname, nickname, title, company, address, home, mobile,"
-                           " work, email, email2, email3, bday, bmonth, byear, aday, amonth, ayear, address2, phone2, notes from addressbook")
+                           " work, email, email2, email3, bday, bmonth, byear, aday, amonth, ayear, address2, phone2, notes from addressbook where deprecated='0000-00-00 00:00:00'")
             for row in cursor:
                 (id, firstname, middlename, lastname, nickname, title, company, address, home, mobile, work, email,
                  email2, email3, bday, bmonth, byear, aday, amonth, ayear, address2, phone2, notes) = row
