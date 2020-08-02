70313679eed86044b0f8a1fe41ad9f8f498adfe3
Jane <yanafinl@gmail.com>
2020-08-02 15:55:04 +0300

    r

--- tests/test_delete_group.py
+++ tests/test_delete_group.py
@@ -1,7 +1,7 @@
 from model.group import Group
 import random
 
-def test_delete_some_group(app, db):
+def test_delete_some_group(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="", header="", footer=""))
     old_group_list = db.get_group_list()
@@ -11,6 +11,8 @@ def test_delete_some_group(app, db):
     assert len(old_group_list) - 1 == len(new_group_list)
     old_group_list.remove(group)
     assert old_group_list == new_group_list
+    if check_ui:
+        assert sorted(new_group_list, key=Group.id_or_max()) == sorted(app.group.get_group_list(), key=Group.id_or_max())
 
 
 
